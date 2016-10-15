25b7ff48e36871a3f2c259418075cfb07c87f601
Sterling Cobb <sterlingcobb@gmail.com>
2016-10-15 01:36:04 -0600

    calculate location on a map

--- index.ios.js
+++ index.ios.js
@@ -2,96 +2,84 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Image,
   StyleSheet,
   Text,
   View,
-  StatusBar,
-  Switch,
-  TouchableOpacity
+  MapView
 } from 'react-native';
+
+function distance(lat1, lon1, lat2, lon2) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    return dist.toFixed(2)
+}
 
 class MyNewApp extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      canLaunch: false,
-      showLaunch: false
+      position : {
+        coords: {
+          latitude: 0,
+          longitude: 0
+        }
+      }
     }
   }
 
-  renderLaunchImage () {
+  distanceFromHome () {
+    const homePosition = {
+      latitude: 40.626523,
+      longitude: -111.885059
+    }
+
+    const currentPosition = this.state.position.coords
+
+    return distance(homePosition.latitude, homePosition.longitude, currentPosition.latitude, currentPosition.longitude)
+  }
+
+  componentDidMount () {
+    const positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 1000
+    }
+
+    const onSuccess = (position) => {
+      this.setState({ position })
+    }
+
+    const onFailure = (error) => {
+      alert(error)
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onFailure, positionOptions)
+    navigator.geolocation.watchPosition(onSuccess, onFailure, positionOptions)
+  }
+
+  render () {
     return (
-      <TouchableOpacity 
-        onPress={(value) => {this.setState({showLaunch: false})}}
-        style = {{flex: 1}}
-      >
-        <Image
-          style={styles.launchImage}
-          source={{uri: "https://s3.amazonaws.com/vigesharing-is-vigecaring/lkurtz/rnwksp-c4-launch.jpg"}}
+      <View style={{flex: 1}}>
+        <MapView 
+          style = {{flex: 1}}
+          showsUserLocation = {true}
+          followUserLocation = {true}
         />
-      </TouchableOpacity>
-    );
-  }
-
-  renderLaunchScreen () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.launchText}>
-          Launch Control
-        </Text>
-
-        <Switch
-          onValueChange = {(value) => {this.setState({canLaunch: value})}}
-          value = {this.state.canLaunch}
-        >
-        </Switch>
-
-        <TouchableOpacity 
-          onPress={(value) => {this.setState({showLaunch: true})}}
-          disabled={!this.state.canLaunch}
-        >
-          <View style={styles.launchButton}>
-            <Text style={styles.launchButtonText}>Launch!</Text>
-          </View>
-        </TouchableOpacity>
+        <View style = {{position: 'absolute', right: 0, left: 0, bottom: 10, padding: 10, backgroundColor: 'rgba(0,0,0,0.5)'}}>
+          <Text style={{textAlign: 'center'}}>{this.distanceFromHome()} miles from home</Text>
+        </View>
       </View>
-    );
-  }
-
-  render() {
-    if (this.state.showLaunch) {
-      return this.renderLaunchImage()
-    } else {
-      return this.renderLaunchScreen()
-    }
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-  },
-  launchText: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 20,
-  },
-  launchButton: {
-    backgroundColor: 'red',
-    margin: 20,
-    padding: 20,
-    borderRadius: 10
-  },
-  launchButtonText: {
-    color: 'white'
-  },
-  launchImage: {
-    flex: 1
-  }
 });
 
 AppRegistry.registerComponent('MyNewApp', () => MyNewApp);
